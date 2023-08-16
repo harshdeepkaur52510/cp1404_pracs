2c88445f77a3ae44bdd3eca00a5fa9f1beca951a
harshdeepkaur52510 <harshdeepkaur@my.jcu.edu.au>
2023-08-16 18:35:50 +1000

    Test file upload

---
+++ prac_06/programming_language.py
@@ -0,0 +1,28 @@
+class ProgrammingLanguage:
+
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        return self.typing == "Dynamic"
+
+
+def test_run():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    languages = [python, ruby, visual_basic]
+    print(python)
+
+    print("The dynamically typed languages are: ")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+if __name__ == "__main__":
+    test_run()
